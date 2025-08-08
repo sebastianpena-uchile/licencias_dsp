3e2b34908e16633259da94e5c71c4578a28709d9
sebastianpena-uchile <sebastianpena@uchile.cl>
2025-08-08 15:50:55 -0400

    agregado años y cie10_familia

---
+++ R/3.crear_variables.R
@@ -0,0 +1,97 @@
+
+# 1. cargar librerias -----------------------------------------------------
+
+library(tidyverse)
+
+
+
+
+
+
+# 2. cargar datos ---------------------------------------------------------
+
+
+
+
+
+
+
+load("data/raw/rechazo_db_raw_ordered.Rdata")
+
+
+
+
+
+
+
+# 3. crear variables ------------------------------------------------------
+
+
+
+# # 2.1. Crear años ----------------------------------------------------
+
+
+
+
+year <- 2013:2023
+
+for (i in year) {
+  # Buscar objetos cuyo nombre termine con el año i
+  objs <- ls(pattern = paste0(i, "$"), envir = .GlobalEnv)
+  
+  for (obj in objs) {
+    df <- get(obj, envir = .GlobalEnv)   # Obtener el data frame
+    df <- cbind(anho = i, df)            # Añadir columna al inicio
+    assign(obj, df, envir = .GlobalEnv)  # Guardar de nuevo con el mismo nombre
+  }
+}
+
+rm(df)
+
+# # 2.2 Familia CIE 10 ----------------------------------------------------
+
+cei10_familia<-c("Ciertas enfermedades infecciosas y parasitarias",
+  "Ciertas enfermedades infecciosas y parasitarias",
+  "Ciertas enfermedades infecciosas y parasitarias",
+  "Trastornos mentales y del comportamiento",
+  "Trastornos mentales y del comportamiento",
+  "Trastornos mentales y del comportamiento",
+  "Enfermedades del sistema respiratorio",
+  "Enfermedades del sistema respiratorio",
+  "Enfermedades del sistema respiratorio",
+  "Enfermedades del sistema digestivo",
+  "Enfermedades del sistema digestivo",
+  "Enfermedades del sistema digestivo",
+  "Enfermedades del sistema osteomuscular y del tejido conectivo",
+  "Enfermedades del sistema osteomuscular y del tejido conectivo",
+  "Enfermedades del sistema osteomuscular y del tejido conectivo",
+  "Traumatismos, envenenamientos y algunas otras consecuencias de causa externa",
+  "Traumatismos, envenenamientos y algunas otras consecuencias de causa externa",
+  "Traumatismos, envenenamientos y algunas otras consecuencias de causa externa",
+  "Otras familias de diagnósticos",
+  "Otras familias de diagnósticos",
+  "Otras familias de diagnósticos",
+  "Total")
+
+year <- 2016:2023
+
+for (i in year) {
+  # Buscar objetos cuyo nombre termine con el año i
+  objs <- ls(pattern = paste0(i, "$"), envir = .GlobalEnv)
+  
+  for (obj in objs) {
+    df <- get(obj, envir = .GlobalEnv)   # Obtener el data frame
+    df <- df %>%
+      mutate(cei10_familia = cei10_familia) %>%
+      relocate(cei10_familia, .after = anho)  # Añadir columna al inicio
+    assign(obj, df, envir = .GlobalEnv)  # Guardar de nuevo con el mismo nombre
+  }
+}
+
+rm(df)
+
+
+
+
+
+
